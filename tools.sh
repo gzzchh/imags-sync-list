24485a6205dd7b750b21ce8c9db3adbbb20fda6f
gzzchh <xjdzch@126.com>
2020-08-25 00:18:31 +0800

    更新忽略与版本

--- tools.sh
+++ tools.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wget -O image-syncer https://github.com/gzzchh/image-syncer/releases/download/1.3.0/image-syncer && chmod +x image-syncer
+wget -O image-syncer https://github.com/gzzchh/image-syncer/releases/download/1.3.1/image-syncer && chmod +x image-syncer
